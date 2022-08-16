a688c34e5a509e451c4e6ae369d2f87e195ed41c
Sergey Kruptsov <serega.by@inbox.ru>
2022-08-16 13:48:46 +0300

    feat: add solution task 2

--- тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
+++ тестовое-задание.курс-frontend-разработка-на-shopify/02-collapse/index.js
@@ -1,1 +1,58 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const collapsibleButtonVisible = document.querySelector('.collapsible__action--visible');
+const collapsibleButtonHidden = document.querySelector('.collapsible__action--hidden');
+const collapsibleContent = document.querySelector('.collapsible__content');
+
+const durationAnimation = 3000
+
+window.addEventListener('load', () => {
+    collapsibleButtonHidden.style.pointerEvents = "none";
+    collapsibleButtonHidden.style.color = "red"
+    collapsibleButtonVisible.style.color = "green"
+})
+
+const hideKeyframes = new KeyframeEffect(
+    collapsibleContent, 
+    [
+        { opacity: '1'},
+        { opacity: '0'},
+        { opacity: '0'},
+        { opacity: '1'}
+    ], 
+    { 
+        duration: durationAnimation,
+        fill: 'forwards' 
+    }
+  );
+
+const hideAnimation = new Animation(hideKeyframes, document.timeline);
+
+
+const showText = () => {
+    collapsibleButtonHidden.style.pointerEvents = "none";
+    collapsibleButtonVisible.style.pointerEvents = "none";
+    hideAnimation.play()
+    setTimeout(()=> {
+        hideAnimation.pause();
+        collapsibleButtonHidden.style.pointerEvents = "auto";
+        collapsibleButtonHidden.style.color = "green"
+        collapsibleButtonVisible.style.color = "red"
+    }, durationAnimation / 2)
+}
+
+const hideText = () => {
+    collapsibleButtonHidden.style.pointerEvents = "none";
+    collapsibleButtonVisible.style.pointerEvents = "none";
+    hideAnimation.play()
+    setTimeout(()=> {
+        hideAnimation.pause();
+        collapsibleButtonVisible.style.pointerEvents = "auto";
+        collapsibleButtonHidden.style.color = "red"
+        collapsibleButtonVisible.style.color = "green"
+    }, durationAnimation / 2)
+    
+}
+
+collapsibleButtonVisible.addEventListener('click', showText)
+collapsibleButtonHidden.addEventListener('click', hideText)
+
+
